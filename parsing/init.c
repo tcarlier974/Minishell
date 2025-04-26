eee4422d84f4fd43145855c362e9ebfbecdc946f
tcarlier <carliertiti97459@gmail.com>
2025-04-26 22:09:00 +0200

    for test

--- parsing/init.c
+++ parsing/init.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 01:21:00 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/04/26 22:05:02 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/04/26 22:08:54 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void init_t_token(t_token *token, char *arg)
 	while (i < get_number_segment(arg) - 1)
 	{
 		tmp->str = NULL;
-		tmp->type = 0;
+		tmp->type = -1;
 		tmp->next = NULL;
 		if (i != 0)
 			tmp->prev = tmp2;
