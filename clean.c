f5eefc1df2213b900d2ebd86843610b1a3734734
tcarlier <carliertiti97459@gmail.com>
2025-04-27 00:44:38 +0200

    ajout clean_extract

--- clean.c
+++ clean.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 23:05:43 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/04/27 00:29:14 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/04/27 00:44:34 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	clean_token(t_token *token)
 	t_token	*tmp;
 	
 	tmp = token;
-	while (tmp->next)
+	while (tmp->next->next)
 	{
 		token = token->next;
 		free(tmp);
