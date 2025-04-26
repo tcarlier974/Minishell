bc2f928aaaa37746116bf39c2613b5c5a0bf7d23
tcarlier <carliertiti97459@gmail.com>
2025-04-27 00:32:22 +0200

    ajout clean_extract

--- parsing/init.c
+++ parsing/init.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 01:21:00 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/04/27 00:30:51 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/04/27 00:32:16 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,15 @@ void init_t_token(t_token *token, char *arg)
 	token->next = tmp;
 	tmp->prev = token;
 	tmp2 = token;
-	while (i < get_number_segment(arg) - 2)
+	while (i < get_number_segment(arg) - 1)
 	{
 		tmp->str = NULL;
 		tmp->type = -1;
 		tmp->next = NULL;
 		if (i != 0)
 			tmp->prev = tmp2;
-		tmp->next = malloc(sizeof(t_token));
+		if (i != get_number_segment(arg) - 1)
+			tmp->next = malloc(sizeof(t_token));
 		tmp2 = tmp;
 		tmp = tmp->next;
 		i++;
