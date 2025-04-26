b1dababa59a175100599d19b1c053f87e119d4f1
tcarlier <carliertiti97459@gmail.com>
2025-04-27 00:29:28 +0200

    ajout clean_extract

--- utils/ft_split.c
+++ utils/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 09:01:33 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/04/27 00:26:53 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/04/27 00:27:54 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,6 @@ char	*ft_strndup(char *s1, int len)
 		i++;
 	}
 	s2[i] = '\0';
-	free(s1);
 	return (s2);
 }
 
