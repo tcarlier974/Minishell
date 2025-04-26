c8f5f7677326bc333e84882d03d39ffd3a3a4af6
tcarlier <carliertiti97459@gmail.com>
2025-04-26 21:59:20 +0200

    for test

--- main.c
+++ main.c
@@ -17,7 +17,7 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
-	t_data	*data = NULL;
+	t_data	data;
 	//char	**args;
 	int		status;
 	(void)argc;
@@ -34,9 +34,9 @@ int	main(int argc, char **argv, char **envp)
 		{
 			add_history(line);
 			printf("%d\n",get_number_segment(line));
-			init(data, line);
+			init(&data, line);
 			write(1, "ok", 2);
-			data->token = tab_to_lst(line, data);
+			data.token = tab_to_lst(line, &data);
 			return (0);
 			//[0] = '\'' [-1] = '\'' = cut and send
 			//[0] = '\"' [-1] = '\"' cut, replace env var calls and send
